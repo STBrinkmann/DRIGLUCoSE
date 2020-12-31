59c4f1271bb913654c8d537e4ce611040bdfce49
Weemaan <bastibrinkmann94@gmail.com>
2020-12-31 02:16:31 +0100

    Erlangen: Rename id -> tag

--- R/Erlangen.R
+++ R/Erlangen.R
@@ -5,7 +5,7 @@
 #'
 #' @format A \code{sf} with 2 column, which are:
 #' \describe{
-#' \item{id}{id}
+#' \item{tag}{id column}
 #' \item{Speed}{Sex and age specific walking speed based on \href{https://ij-healthgeographics.biomedcentral.com/articles/10.1186/1476-072X-11-43}{Dewulf et al.}.}
 #' }
 "Erlangen"
